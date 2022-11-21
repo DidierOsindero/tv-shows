24e12d3dd9560e50fcfad0db97d9956521bee552
Didier Osindero <didierosindero@gmail.com>
2022-11-21 15:12:52 +0000

    Write functionality for episodeCodeFormatter

--- src/utils/episodeCodeFormatter.ts
+++ src/utils/episodeCodeFormatter.ts
@@ -1,11 +1,14 @@
-export const episodeCodeFormatter = (seasonNum: number, episodeNum: number): string => {
-    
-    
-    return ""
-}
+export const episodeCodeFormatter = (
+  seasonNum: number,
+  episodeNum: number
+): string => {
+  return `S${seasonNum.toLocaleString("en-UK", {
+    minimumIntegerDigits: 2,
+  })}E${episodeNum.toLocaleString("en-UK", { minimumIntegerDigits: 2 })}`;
+};
 
 let number = 20;
-console.log(number.toLocaleString("en-UK", {minimumIntegerDigits: 2}))
+console.log(number.toLocaleString("en-UK", { minimumIntegerDigits: 2 }));
 
 /*
 S02E07 would be the code for the 7th episode of the 2nd season. S2E7 would be incorrect.
