d5ad8840207f4f1f6c05067e874247a44458fa8b
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-11-21 12:11:23 +0000

    formated and linted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-  import episodesData from './episodes.json'
+import episodesData from "./episodes.json";
 import { greet } from "./utils/greet";
+
+console.log(`Imported ${episodesData.length} episode(s)`);
+console.log(`First episode's name is ${episodesData[0].name}`);
 
 function App(): JSX.Element {
   return <h1>{greet("World")}</h1>;
