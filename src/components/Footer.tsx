a38bb98e15bbeb058cb55a87e820a90b31300179
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-11-21 15:39:45 +0000

    add link to data provider

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,10 @@
 export const Footer = (): JSX.Element => {
   return (
     <div className="footerContainer">
-      <h4>Footer</h4>
+      <hr />
+      <h4>
+        All data has been proved by <a href="https://tvmaze.com/">TVMaze.com</a>
+      </h4>
     </div>
   );
 };
