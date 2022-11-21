4b8cce005e09f62cf3ccaa710529b22034835537
Didier Osindero <didierosindero@gmail.com>
2022-11-21 13:47:04 +0000

    Episode component structure

--- src/components/Episode.tsx
+++ src/components/Episode.tsx
@@ -1,7 +1,20 @@
-export const Episode = (): JSX.Element => {
-    return (
-      <div className="episodeContainer">
-        <h1>My App</h1>
+import { IEpisode } from "./MainContent";
+
+export const Episode = (props: IEpisode): JSX.Element => {
+  return (
+    <div className="episodeContainer">
+      <h4>
+        {props.name} -{" "}
+        <i>
+          {props.season}: {props.number}
+        </i>
+      </h4>
+      <div className="episodeImageContainer">
+        <img src={props.image.medium} />
       </div>
-    )
-  };
+      <div className="episodeSummaryContainer">
+        <p>{props.summary}</p>
+      </div>
+    </div>
+  );
+};
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,8 +1,29 @@
 import { EpisodesListView } from "./EpisodesListView";
-export const MainContent = (): JSX.Element => {
-    return (
-      <div className="mainContentContainer">
-        <p>Main Content</p>
-      </div>
-    )
+
+export interface IEpisode {
+  id: number;
+  url: string;
+  name: string;
+  season: number;
+  number: number;
+  type: string;
+  airdate: string;
+  airtime: string;
+  airstamp: string;
+  rating: { average: number };
+  runtime: number;
+  image: {
+    medium: string;
+    original: string;
   };
+  summary: string;
+  _links: { self: { href: string } };
+}
+
+export const MainContent = (): JSX.Element => {
+  return (
+    <div className="mainContentContainer">
+      <p>Main Content</p>
+    </div>
+  );
+};
