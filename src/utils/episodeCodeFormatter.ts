91f6f4905b38041b34b2c15013009ec4cd35a07c
Didier Osindero <didierosindero@gmail.com>
2022-11-21 15:25:12 +0000

    Remove uneccessary console.log

--- src/utils/episodeCodeFormatter.ts
+++ src/utils/episodeCodeFormatter.ts
@@ -7,9 +7,6 @@ export const episodeCodeFormatter = (
   })}E${episodeNum.toLocaleString("en-UK", { minimumIntegerDigits: 2 })}`;
 };
 
-let number = 20;
-console.log(number.toLocaleString("en-UK", { minimumIntegerDigits: 2 }));
-
 /*
 S02E07 would be the code for the 7th episode of the 2nd season. S2E7 would be incorrect.
 */
