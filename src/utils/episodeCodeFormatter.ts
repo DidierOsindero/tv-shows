8109a593cb35aa64922420e621ebd088448dd584
Didier Osindero <didierosindero@gmail.com>
2022-11-21 15:05:16 +0000

    Add episodeCodeFormatter file to utils

---
+++ src/utils/episodeCodeFormatter.ts
@@ -0,0 +1,12 @@
+export const episodeCodeFormatter = (seasonNum: number, episodeNum: number): string => {
+    
+    
+    return ""
+}
+
+let number = 20;
+console.log(number.toLocaleString("en-UK", {minimumIntegerDigits: 2}))
+
+/*
+S02E07 would be the code for the 7th episode of the 2nd season. S2E7 would be incorrect.
+*/
