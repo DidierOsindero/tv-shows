97d05e3345ce35d3c14b4d1bd0ed9cdd7a0d90ec
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-11-22 11:37:31 +0000

    add tests for remove p tags function

---
+++ src/utils/removeParagraphsTags.test.ts
@@ -0,0 +1,8 @@
+import { removeParagraphTags } from "./removeParagraphTags";
+
+
+test('removeParagraphTags should remove any html p tags', () => {
+    expect(removeParagraphTags("<p>World</p>")).toBe("World");
+    expect(removeParagraphTags("<p>Richard is my name</p>")).toBe("Richard is my name");
+    expect(removeParagraphTags("Academy Scholars")).toBe("Academy Scholars");
+  });
