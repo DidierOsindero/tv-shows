5dd873da428d35292ae8751106f82f1053a227d3
Didier Osindero <didierosindero@gmail.com>
2022-11-22 14:33:07 +0000

    Fetch showData

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 //import episodesData from "./episodes.json";
 import "./App.css";
 import { Header } from "./components/Header";
-import { IEpisode, MainContent } from "./components/MainContent";
+import { IEpisode } from "./interfaces";
+import { IShow } from "./interfaces";
+import { MainContent } from "./components/MainContent";
 import { Footer } from "./components/Footer";
 import { useEffect, useState } from "react";
 
+
 function App(): JSX.Element {
   const [episodesData, setEpisodesData] = useState<IEpisode[]>([]);
+  const [showData, setShowData] = useState<IShow[]>([]);
 
   const fetchEpisodesData = async () => {
     const response = await fetch("https://api.tvmaze.com/shows/82/episodes");
@@ -14,14 +18,23 @@ function App(): JSX.Element {
     setEpisodesData(fetchedDataJSON);
   };
 
+  const fetchShowData = async () => {
+    const response = await fetch("http://api.tvmaze.com/shows?page=1");
+    const fetchedDataJSON = await response.json();
+    setShowData(fetchedDataJSON);
+  };
+
   useEffect(() => {
     fetchEpisodesData();
+    fetchShowData();
   }, []);
+
+ 
 
   return (
     <div className="appContainer">
       <Header />
-      <MainContent episodesData={episodesData} />
+      <MainContent episodesData={episodesData} showData={showData}/>
       <Footer />
     </div>
   );
