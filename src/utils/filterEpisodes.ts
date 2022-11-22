8a7b7308031e105313bde09725086c03904017a9
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-11-22 16:41:25 +0000

    dont render summary for episodes without summary.

--- src/utils/filterEpisodes.ts
+++ src/utils/filterEpisodes.ts
@@ -7,7 +7,8 @@ export const filterEpisodes = (
   const filteredArr: IEpisode[] = episodes.filter((el) => {
     const isMatchingSearchText =
       el.name.toLowerCase().includes(currentSearchText.toLowerCase()) ||
-      el.summary.toLowerCase().includes(currentSearchText.toLowerCase());
+      (el.summary &&
+        el.summary.toLowerCase().includes(currentSearchText.toLowerCase()));
     return isMatchingSearchText;
   });
   return filteredArr;
