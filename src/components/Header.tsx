1822f62ebd19bfb9b82166258ab8e9aff04b6c6f
DidierOsindero <84969980+DidierOsindero@users.noreply.github.com>
2023-01-10 11:47:41 +0000

    Fixed nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = (): JSX.Element => {
   return (
     <div className="headerContainer">
-      <h1 className="title">Tv Shows</h1>
+      <h1 className="title" onClick={() => window.scrollTo({ top: 0 })}>Tv Shows</h1>
     </div>
   );
 };
